90211178534efaee066e8d135be620da7cb62c90
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-05 20:35:20 +0200

    Created solution

---
+++ rcmco/rcmco.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rcmco.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
