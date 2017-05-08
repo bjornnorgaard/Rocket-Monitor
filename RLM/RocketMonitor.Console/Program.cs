5688d0b9a39732025265ae894066465bccf5b9e0
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-08 02:07:23 +0200

    Made some outputs for users

--- RLM/RocketMonitor.Console/Program.cs
+++ RLM/RocketMonitor.Console/Program.cs
@@ -9,9 +9,35 @@ namespace RocketMonitor.Console
             var rocketMonitor = new RocketMonitor();
 
             var filePath = Environment.GetEnvironmentVariable("USERPROFILE") + "\\Documents\\my games\\Rocket League\\TAGame\\Config\\TASystemSettings.ini";
-            var newResolution = "3840";
+            const int fullHd = 1920;
+            int numberOfPlayers;
 
-            rocketMonitor.ChangeResolution(filePath, newResolution);
+            System.Console.Write("Number of players: ");
+
+            try
+            {
+                numberOfPlayers = int.Parse(System.Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                System.Console.WriteLine("... NUMBER of players. Not letter of player, dumb user.");
+                System.Threading.Thread.Sleep(2000);
+                return;
+            }
+
+            var newResolution = numberOfPlayers * fullHd;
+
+            if (0 < numberOfPlayers && numberOfPlayers < 5)
+            {
+                System.Console.WriteLine("Setting new game resolution...");
+                rocketMonitor.ChangeResolution(filePath, newResolution.ToString());
+                System.Console.WriteLine("Now you can launch the game.");
+                System.Threading.Thread.Sleep(2000);
+                return;
+            }
+
+            System.Console.WriteLine("Oops. Something didn't workout.");
+            System.Threading.Thread.Sleep(2000);
         }
     }
 }
