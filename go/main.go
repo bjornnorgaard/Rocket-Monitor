3803cb403c53864af561e524d20c9a955afdd5c2
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2018-09-17 20:06:20 +0200

    Not done, but getting there

--- go/main.go
+++ go/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	path := "TASystemSettings.ini"
+	fullscreen := "Fullscreen=True"
+	borderless := "Borderless=False"
+	resx := "ResX=1920"
+
+	dat, err := ioutil.ReadFile(path)
+	check(err)
+	lines := strings.Split(string(dat), "\n")
+
+	for _, x := range lines {
+		if strings.Contains(x, fullscreen) {
+			fmt.Println(x)
+		}
+		if strings.Contains(x, borderless) {
+			fmt.Println(x)
+		}
+		if strings.Contains(x, resx) {
+			fmt.Println(x)
+		}
+	}
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
